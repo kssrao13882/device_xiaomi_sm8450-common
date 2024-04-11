20c1fc2cba19ac9471e044c003c3e15237d7a0e0
chaitanyakm <chaitanyaarora3@gmail.com>
2024-04-11 23:22:41 +0530

    sm8450-common: parts: Drop Thermal Profiles
    its reducing performance lol

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,7 +23,6 @@ import android.content.Intent;
 import android.util.Log;
 
 import org.lineageos.settings.doze.DozeUtils;
-import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -35,7 +34,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
         DozeUtils.onBootCompleted(context);
-        ThermalUtils.startService(context);
         RefreshUtils.startService(context);        
     }
 }
