8247cb82959657f604507ad3a14440c4df489a29
kssrao13882 <kssrao13882@gmail.com>
2024-05-08 09:31:41 +0530

    Revert "sm8450-common: parts: Drop Thermal Profiles"
    This reverts commit 20c1fc2cba19ac9471e044c003c3e15237d7a0e0.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,6 +23,7 @@ import android.content.Intent;
 import android.util.Log;
 
 import org.lineageos.settings.doze.DozeUtils;
+import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -34,6 +35,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
         DozeUtils.onBootCompleted(context);
+        ThermalUtils.startService(context);
         RefreshUtils.startService(context);        
     }
 }
